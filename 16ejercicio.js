c7d616deb6fa8ed55c93935d170c5427d7c55b23
Luis Izquierdo <a329630@uach.mx>
2021-04-27 18:36:53 -0600

    Imprimir diez veces la serie de numeros del 1 al 10

---
+++ 16ejercicio.js
@@ -0,0 +1,8 @@
+// imprimir diez veces la serie de numeros del 1 al 10
+
+for (let i = 1; i <= 10; i++) {
+    for (let j = 0; j <= 10; j++) {
+        console.log(j);
+    }
+    console.log(`------------ ${i}`);  
+}
