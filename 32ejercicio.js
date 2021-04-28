7ad264c4d3e358d50356cc578066c8ae63e0d83f
Luis Izquierdo <a329630@uach.mx>
2021-04-27 23:52:34 -0600

    Arreglo de 20 nombres

---
+++ 32ejercicio.js
@@ -0,0 +1,9 @@
+// arreglo de 20 nombres de personas
+
+let names = ["Luis", "Andres", "Sebastian", "Humberto", "Sandra",
+            "Alejandro", "David", "Maria", "Fernanda", "Marina",
+            "Axel", "Maritrini", "Daniel", "Gabriel", "Patricia",
+            "Juan", "Jesus", "Roberto", "Jessica", "Abril"];
+
+for (let index = 0; index < 20; index++) 
+    console.log(names[index]);    
